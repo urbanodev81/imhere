08be00d0f8142a587f1723d6b8f905e16637682c
Ricardo Urbano <ricardo.urbano@SEDE.CEJAM>
2025-02-13 18:41:00 -0300

    fim aula - 2 - botão de adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,10 @@ import {styles} from './styles'
 export function Home() {
      const participants = ['22222', '33333', '44444','55555','1111111','6666666','7777777','888888','9999999','10101010'];
 
+    function handleParticipantsAdd(){
+        alert("teste");
+    }
+
     return (
         <View style={styles.container}>
             <Text key='1' style={styles.eventName}>Open up App.tsx to stdddart working on your app!33</Text>
@@ -11,6 +15,14 @@ export function Home() {
                 color:'red',
                 fontSize: 16
             }}>Open up App.tsx to start wssorking on your</Text>
+            <TextInput style={styles.input} 
+                placeholder='Nome do participante'
+                placeholderTextColor="#6b6b6b"/>
+            <TouchableOpacity style={styles.button} onPress={handleParticipantsAdd}>
+                <Text style={styles.buttonText}>
+                    +
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
